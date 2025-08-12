6edce8a70e76f50be46afa72c530774470b7be76
Nathaniel Hamming <nathaniel.hamming@gmail.com>
2025-08-12 08:52:51 -0300

    point to dev in the BluetoothCommonKit

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
             targets: ["InsulinDeliveryServiceKit"]),
     ],
     dependencies: [
-        .package(url: "git@github.com:tidepool-org/BluetoothCommonKit.git", branch: "nate/IDS-refactoring")
+        .package(url: "git@github.com:tidepool-org/BluetoothCommonKit.git", branch: "dev")
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
