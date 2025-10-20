f9a59b9db8d3e094b568f19f0602958b0c8c5fee
Nathaniel Hamming <nathaniel.hamming@gmail.com>
2025-10-20 08:45:01 -0300

    fixing e2e protection

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
             targets: ["InsulinDeliveryServiceKit"]),
     ],
     dependencies: [
-        .package(url: "git@github.com:tidepool-org/BluetoothCommonKit.git", branch: "dev")
+        .package(url: "git@github.com:tidepool-org/BluetoothCommonKit.git", branch: "nate/fix-e2e-protection")
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
